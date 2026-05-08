74dbf87c539ab8395423f173b86b8c797b4b13af
gamza <lee77347559@gmail.com>
2026-05-08 09:17:05 +0900

    First Setting
    깃허브 세팅

---
+++ Prontend/test/widget_test.dart
@@ -0,0 +1,22 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:ticketdiary/main.dart';
+
+void main() {
+  testWidgets('shows diary home screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const TicketDiaryApp());
+
+    expect(find.text('티켓  추가'), findsOneWidget);
+    expect(find.text('배송전'), findsWidgets);
+    expect(find.text('공연전'), findsWidgets);
+    expect(find.text('공연후'), findsWidgets);
+  });
+}
